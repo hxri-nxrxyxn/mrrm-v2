1e8f0d2c1428c965e321e1305aeee2d9cc3d49a9
Hari Narayan <harinarayanmr@aol.com>
2026-01-16 00:12:20 +0530

    feat: significant changes, packaged for android

---
+++ frontend/v2/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.saintgits.mrrm',
+  appName: 'MRRM',
+  webDir: 'dist'
+};
+
+export default config;
